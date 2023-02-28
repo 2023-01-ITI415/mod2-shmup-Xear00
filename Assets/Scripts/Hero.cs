6faa97ad275e611117a8772bd80ca2560c9a9a23
Xear <zzier42@gmail.com>
2023-02-27 20:26:35 -0500

    Added Ship Destruction

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -12,8 +12,12 @@ public class Hero : MonoBehaviour
     public float rollMult = -45;
     public float pitchMult = 30;
 
-    [Header("Dynamic")] [Range(0,4)]
-    public float shieldLevel = 1;
+    [Header("Dynamic")] [Range(0,4)] [SerializeField]
+    private float _shieldLevel = 1;
+    //public float shieldLevel = 1;
+
+    [Tooltip("This field holds a reference to the last triggering GameObject")]
+    private GameObject lastTriggerGo = null; 
 
     void Awake(){
         if (S == null){
@@ -44,5 +48,30 @@ public class Hero : MonoBehaviour
         Transform rootT = other.gameObject.transform.root;
         GameObject go = rootT.gameObject;
         Debug.Log("Shield trigger hit by: " +go.gameObject.name);
+
+        //make sure its not the same triggering go as last time
+        if (go == lastTriggerGo) return;
+        lastTriggerGo = go;
+
+        Enemy enemy = go.GetComponent<Enemy>();
+        if (enemy !=null){
+            shieldLevel--;
+            Destroy(go);
+        }
+         else{
+            Debug.LogWarning("Shield trigger hit by non-Enemy: "+go.name);
+         }
     }
+
+    public float shieldLevel{
+        get {return (_shieldLevel); }
+        private set {
+            _shieldLevel = Mathf.Min(value, 4);
+
+            if (value < 0) {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
 }
